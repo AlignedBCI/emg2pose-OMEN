d8e5144a0cc402aee3330ccf7c409d49f26ccdfb
Federico Claudi <claudif@mit.edu>
2025-02-05 09:02:31 -0500

    bump

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 import os
+from pathlib import Path
 
+DATA_DIR = "F:/emg2pose/emg2pose_dataset_mini"
+if not Path(DATA_DIR).exists():
+    DATA_DIR = "/om2/user/claudif/DecodingAlgorithms/datasets/emg2pose/emg2pose_dataset_mini"
 
 # set HYDRA_FULL_ERROR=1
 os.environ["HYDRA_FULL_ERROR"] = "1"
 
-cmd = """
-python -m emg2pose.train train=True eval=True experiment=tracking_omen trainer.max_epochs=5 data_split=mini_split data_location="F:/emg2pose/emg2pose_dataset_mini"
+cmd = f"""
+python -m emg2pose.train train=True eval=True experiment=tracking_omen trainer.max_epochs=5 data_split=mini_split data_location="{DATA_DIR}"
 """
 
 os.system(cmd)
