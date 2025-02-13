038f047e6f6be3adf801144a1a40174baec9f94a
Federico Claudi <claudif@mit.edu>
2025-02-12 21:32:42 -0500

    bump

--- emg2pose/lightning.py
+++ emg2pose/lightning.py
@@ -9,7 +9,7 @@ import logging
 
 from collections.abc import Mapping, Sequence
 from pathlib import Path
-
+from loguru import logger as lgr
 import pytorch_lightning as pl
 import torch
 
@@ -99,6 +99,11 @@ class WindowedEmgDataModule(pl.LightningDataModule):
             ]
         )
 
+        # Print dataset sizes
+        lgr.info(f"Train dataset size: {len(self.train_dataset)}")
+        lgr.info(f"Val dataset size: {len(self.val_dataset)}")
+        lgr.info(f"Test dataset size: {len(self.test_dataset)}")
+
     def train_dataloader(self) -> DataLoader:
         return DataLoader(
             self.train_dataset,
@@ -170,18 +175,22 @@ class Emg2PoseModule(pl.LightningModule):
         for loss_name, weight in self.loss_weights.items():
             loss += metrics[f"{stage}_{loss_name}"] * weight
         self.log(f"{stage}_loss", loss, sync_dist=True, prog_bar=True)
+        
 
         return loss
 
     def training_step(self, batch, batch_idx) -> torch.Tensor:
+        lgr.info(f"Training step {batch_idx}")
         return self._step(batch, stage="train")
 
     def validation_step(self, batch, batch_idx) -> torch.Tensor:
+        lgr.info(f"Validation step {batch_idx}")
         return self._step(batch, stage="val")
 
     def test_step(
         self, batch, batch_idx, dataloader_idx: int = None
     ) -> torch.Tensor:
+        lgr.info(f"Test step {batch_idx}")
         return self._step(batch, stage="test")
 
     def configure_optimizers(self):
