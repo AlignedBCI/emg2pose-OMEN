44e7c8a96e1735b1c0bf11eb838a64d02d0e9f59
Federico Claudi <claudif@mit.edu>
2025-02-16 08:57:41 -0500

    bump

--- train.sh
+++ train.sh
@@ -7,7 +7,7 @@
 #SBATCH --mem=150GB                   
 #SBATCH -o emg2pose_omen_%j.log
 #SBATCH -e emg2pose_omen_%j.err
-#SBATCH --gres=gpu:4 --constraint=12GB
+#SBATCH --gpus-per-node=4
 
 
 cd /om2/user/claudif/DecodingAlgorithms/emg2pose-OMEN
